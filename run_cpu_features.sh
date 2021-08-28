e68634dacfd3b6c980231200dbc8b884da6b4479
Xinyi Wang <49368856+WXYIIIIII@users.noreply.github.com>
2021-08-28 23:34:25 +0100

    Update run_cpu_features.sh

--- run_cpu_features.sh
+++ run_cpu_features.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=cnn1080
+#SBATCH --job-name=1080p5
 #SBATCH --partition=cpu
-#SBATCH --nodes=16
-#SBATCH --ntasks=8
+#SBATCH --nodes=4
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=1
-#SBATCH -o /mnt/storage/home/um20242/scratch/VSFA-master/logs/cnn1080.out
-#SBATCH --mem-per-cpu=80G
+#SBATCH -o /mnt/storage/home/um20242/scratch/VSFA-UGC/logs/1080-p5.out
+#SBATCH --mem-per-cpu=120G
 
 cd "${SLURM_SUBMIT_DIR}"
 
@@ -31,11 +31,11 @@ source activate reproducibleresearch        #pip install -r requirements.txt -i 
 # test demo video sequence
 #python test_demo.py --video_format=RGB --video_path=/mnt/storage/home/um20242/scratch/test_sequence/original_test.mp4
 #python test_demo.py --video_format=RGB --video_path=/mnt/storage/home/um20242/scratch/test_sequence/decoded_test.mp4
-#python test_demo.py --video_format=RGB --video_path=/mnt/storage/home/um20242/scratch/VSFA-master/test.mp4
+#python test_demo.py --video_format=RGB --video_path=/mnt/storage/home/um20242/scratch/VSFA-UGC/test.mp4
 
 # CNN features extraction
 #nohup python -u CNNfeatures.py>test.log 2>&1 &
-CUDA_VISIBLE_DEVICES=2 python CNNfeaturesUGC.py --database=YOUTUBE_UGC_1080P --frame_batch_size=32
+CUDA_VISIBLE_DEVICES=2 python CNNfeaturesUGC.py --database=YOUTUBE_UGC_1080P_p5 --frame_batch_size=32
 
 
 ## Deactivate virtualenv
